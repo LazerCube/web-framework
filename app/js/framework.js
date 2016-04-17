adb23831efd579c453b8d25d34399687c86b72ab
LazerCube <git@lazercube.com>
2016-04-17 17:53:54 +0100

    Fixed issuse with mobile navbar closing on menu click

--- app/js/framework.js
+++ app/js/framework.js
@@ -10,7 +10,7 @@ $(function(){
     $('body').on('click',function(event){
         if ($(".nav_trigger").css("visibility") == "visible" ){
             if($("body").hasClass("show_sidebar")){
-                if($(event.target).not('#push_sidebar')){
+                if(!$(event.target).is('#push_sidebar')){
                     event.stopPropagation();
                     $("body").removeClass("show_sidebar");
                     console.log("No Clicking on push_sidebar");
