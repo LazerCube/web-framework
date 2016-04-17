4d1eadc17c28a713128506f82615cec0cc7b4df1
LazerCube <git@lazercube.com>
2016-04-17 17:35:09 +0100

    Added a way to close the sidebar on mobile.

--- app/js/framework.js
+++ app/js/framework.js
@@ -1,35 +1,41 @@
 $(document).ready(function() {
     // run test on initial page load
-
     checkSize();
 
     // run test on resize of the window
-
     $(window).resize(checkSize);
 });
 
-//Function for the sidebar
 $(function(){
+    $('body').on('click',function(event){
+        if ($(".nav_trigger").css("visibility") == "visible" ){
+            if($("body").hasClass("show_sidebar")){
+                if($(event.target).not('#push_sidebar')){
+                    event.stopPropagation();
+                    $("body").removeClass("show_sidebar");
+                    console.log("No Clicking on push_sidebar");
+                }
+            }
+        }
+    });
+
     $(".nav_trigger").click(function() {
-        toggleSideNav();
+        console.log("TOGGLE");
+        event.stopPropagation();
+        $("body").addClass("show_sidebar");
     });
 });
-
-function toggleSideNav(){
-    $("body").toggleClass("show_sidebar");
-}
 
 //Function to the css rule
 function checkSize(){
     if ($(".nav_trigger").css("visibility") == "visible" ){
-        if($( "body" ).hasClass( "show_sidebar" ))
-        {
-            toggleSideNav();
+        if($("body").hasClass( "show_sidebar" )){
+            $("body").removeClass("show_sidebar");
         }
     }
     else{
         if($( "body" ).hasClass( "" )){
-            toggleSideNav();
+            $("body").addClass("show_sidebar");
         }
     }
 }
